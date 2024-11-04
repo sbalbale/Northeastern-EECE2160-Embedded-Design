dd02eca18079f4fdf20abb4ae2b10350881723de
Sean Balbale <sean.balbale@gmail.com>
2024-11-04 15:10:49 -0500

    labs

--- C++/helloworld/test.cpp
+++ C++/helloworld/test.cpp
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main ()
+
+int main()
 {
-printf("Hello AskUbuntu!");
+    char initials[3]; // Assuming initials are two characters plus the null terminator
+    printf("Enter your initials: ");
+    scanf("%2s", initials); // Read up to 2 characters
+    printf("Hello %s\n", initials);
+    return 0;
 }
