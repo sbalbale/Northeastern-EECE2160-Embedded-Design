b76ae598a4d3d791a4e8c8d3b470d7f7c95ceb9d
Sean Balbale <sean.balbale@gmail.com>
2024-11-04 15:53:17 -0500

    lab

---
+++ Labs/Lab6/hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+  char fi;
+  char si;
+  
+  std::cout<<"give me your first initial"<<std::endl;
+  std::cin>> fi;
+  
+  std::cout<<"give me your second initial"<<std::endl;
+  std::cin>> si;
+  
+  std::cout<<"hello "<<fi<<" "<<si<<std::endl;
+}
